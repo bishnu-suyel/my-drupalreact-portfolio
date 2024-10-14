51b55eb92c81eb9583448ccda5d19dbe8c5d5490
Suyel Bishnu <suyel2007@gmail.com>
2024-10-15 01:25:50 +0300

    Use react bootstrap for  styling

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,32 +1,44 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Navbar, Nav, Container } from "react-bootstrap";
 
 const Layout = ({ children }) => {
   return (
     <div>
-      <header>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/projects">Projects</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/blog">Blog</Link>
-            </li>
-            <li>
-              <Link to="/contact">Contact</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
+      {/* Bootstrap Navbar */}
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <Container>
+          <Navbar.Brand as={Link} to="/">
+            Bishnu Suyel
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to="/">
+                Home
+              </Nav.Link>
+              <Nav.Link as={Link} to="/projects">
+                Projects
+              </Nav.Link>
+              <Nav.Link as={Link} to="/about">
+                About
+              </Nav.Link>
+              <Nav.Link as={Link} to="/blog">
+                Blog
+              </Nav.Link>
+              <Nav.Link as={Link} to="/contact">
+                Contact
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      {/* Main Content */}
       <main>{children}</main>
-      <footer>
+
+      {/* Footer */}
+      <footer className="text-center mt-4">
         <p>
           &copy; {new Date().getFullYear()} Bishnu Suyel. All rights reserved.
         </p>
