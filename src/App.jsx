3198195deff843990ae8b57cc68737c1eb88000e
Suyel Bishnu <suyel2007@gmail.com>
2024-10-14 13:00:21 +0300

    Create route path for new components

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,19 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
+import Project from "./components/Project";
+import Contact from "./components/Contact";
+import Blog from "./components/Blog";
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Project />} />
         <Route path="/about" element={<About />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </Router>
   );
