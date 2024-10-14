0dd61855db6b43dd0271ac71ca5452cbd11199db
Suyel Bishnu <suyel2007@gmail.com>
2024-10-15 01:27:21 +0300

    Change the api end point

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = "http://localhost:49859/jsonapi/";
+const API_URL = "http://localhost:49934/jsonapi/";
 
 export const fetchContent = async (contentType) => {
   const response = await axios.get(`${API_URL}/${contentType}`);
--- src/services/blogApi.jsx
+++ src/services/blogApi.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:49859/jsonapi/";
+const API_URL = "http://localhost:49934/jsonapi/";
 
 export const fetchContent = async () => {
   try {
